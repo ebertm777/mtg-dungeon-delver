18cfc2ac4fd95f442c871c0f4b3373eef56e3fe7
ebertm777 <eber.tmacedo@gmail.com>
2024-10-08 15:38:53 -0300

    added click logic

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,14 +1,61 @@
-import React from 'react';
-
-import * as S from './styles';
+import React, { useState } from "react";
+import * as S from "./styles";
 
 const Main = () => {
+  // state to track active room
+  const [activeRoom, setActiveRoom] = useState("");
+
+  // Function to handle room click
+  const handleRoomClick = (room) => {
+    setActiveRoom(room);
+  };
+
+  // Conditionally apply styles based on activeRoom
+  const getRoomStyle = (room) => {
+    return room === activeRoom
+      ? {
+          backgroundColor: "rgb(212,175,55)",
+          opacity: 0.4,
+        }
+      : {};
+  };
 
   return (
     <S.Container>
-<S.Dungeon>
-  <S.Entrance/>
-</S.Dungeon>
+      <S.Dungeon>
+        <S.Entrance
+          style={getRoomStyle("entrance")}
+          onClick={() => handleRoomClick("entrance")}
+        />
+        <S.SecondFloor>
+          <S.GoblinLair
+            style={getRoomStyle("goblinLair")}
+            onClick={() => handleRoomClick("goblinLair")}
+          />
+          <S.MineTunnels
+            style={getRoomStyle("mineTunnels")}
+            onClick={() => handleRoomClick("mineTunnels")}
+          />
+        </S.SecondFloor>
+        <S.ThirdFloor>
+          <S.Storeroom
+            style={getRoomStyle("storeroom")}
+            onClick={() => handleRoomClick("storeroom")}
+          />
+          <S.DarkPool
+            style={getRoomStyle("darkPool")}
+            onClick={() => handleRoomClick("darkPool")}
+          />
+          <S.FungiCavern
+            style={getRoomStyle("fungiCavern")}
+            onClick={() => handleRoomClick("fungiCavern")}
+          />
+        </S.ThirdFloor>
+        <S.TempleOfDumathoin
+          style={getRoomStyle("templeOfDumathoin")}
+          onClick={() => handleRoomClick("templeOfDumathoin")}
+        />
+      </S.Dungeon>
     </S.Container>
   );
 };
--- src/Main/styles.js
+++ src/Main/styles.js
@@ -25,6 +25,59 @@ export const Entrance = styled.div`
   height: 85px;
   margin-left: 40px;
   margin-top: 93px;
-  background-color: red;
-  opacity: 0.4;
+  cursor: pointer;
+`;
+
+export const SecondFloor = styled.div`
+  width: 362px;
+  height: 110px;
+  display: flex;
+  flex-direction: row;
+  margin-left: 40px;
+  margin-top: 9px;
+`;
+
+export const GoblinLair = styled.div`
+  width: 178px;
+  cursor: pointer;
+`;
+
+export const MineTunnels = styled.div`
+  width: 178px;
+  margin-left: 6px;
+  cursor: pointer;
+`;
+
+export const ThirdFloor = styled.div`
+  width: 362px;
+  height: 135px;
+  display: flex;
+  flex-direction: row;
+  margin-left: 40px;
+  margin-top: 8px;
+`;
+
+export const Storeroom = styled.div`
+  width: 107px;
+  cursor: pointer;
+`;
+
+export const DarkPool = styled.div`
+  width: 130px;
+  cursor: pointer;
+  margin-left: 10px;
+`;
+
+export const FungiCavern = styled.div`
+  width: 109px;
+  cursor: pointer;
+  margin-left: 9px;
+`;
+
+export const TempleOfDumathoin = styled.div`
+  width: 362px;
+  height: 81px;
+  cursor: pointer;
+  margin-left: 40px;
+  margin-top: 10px;
 `;
